2e14237e40c36ec36810741eda0a5dce84021a79
Benjamin Stauffer <kcowolf@gmail.com>
2021-02-15 22:28:21 -0600

    Allow multiple files to be parsed

--- src/main.cpp
+++ src/main.cpp
@@ -13,47 +13,37 @@ int main(int argc, char* argv[])
 {
     printf("GenImageTool - v%s\n\n", VERSION);
 
-    std::filesystem::path graphicsDir;
-    std::filesystem::path outputDir;
-    std::filesystem::path inputFile;
-
     try
     {
         TCLAP::CmdLine cmdLine{ "", ' ', VERSION };
         TCLAP::ValueArg<std::string> graphicsDirArg{ "g", "graphicsDir", "Directory where image files are located (defaults to same directory as input file)", false, "", "directory" };
         TCLAP::ValueArg<std::string> outputDirArg{ "o", "outputDir", "Directory where files will be written (defaults to same directory as input file)", false, "", "directory" };
-        TCLAP::UnlabeledValueArg<std::string> inputFileArg{ "inputFile", "Text file to be parsed", true, "", "filename" };
+        TCLAP::UnlabeledMultiArg<std::string> inputFileArgs{ "inputFile", "One or more text files to be parsed", true, "filename" };
 
         cmdLine.add(graphicsDirArg);
         cmdLine.add(outputDirArg);
-        cmdLine.add(inputFileArg);
+        cmdLine.add(inputFileArgs);
         cmdLine.parse(argc, argv);
 
-        inputFile = std::filesystem::absolute(inputFileArg.getValue());
+        for (std::string inputFileArg : inputFileArgs.getValue())
+        {
+            printf("Parsing %s\n", inputFileArg.c_str());
 
-        graphicsDir = graphicsDirArg.getValue().empty() ? inputFile.parent_path() : graphicsDirArg.getValue();
-        outputDir = outputDirArg.getValue().empty() ? inputFile.parent_path() : outputDirArg.getValue();
+            std::filesystem::path inputFile = std::filesystem::absolute(inputFileArg);
+            std::filesystem::path graphicsDir = graphicsDirArg.getValue().empty() ? inputFile.parent_path() : graphicsDirArg.getValue();
+            std::filesystem::path outputDir = outputDirArg.getValue().empty() ? inputFile.parent_path() : outputDirArg.getValue();
+
+            GenImageTool::Parser parser;
+            GenesisObjects genesisObjects = parser.parse(inputFile, graphicsDir);
+
+            GenImageTool::Writer writer;
+            writer.write(genesisObjects, outputDir);
+        }
     }
     catch (TCLAP::ArgException& e)
     {
         fprintf(stderr, "[ERROR] %s for arg %s\n", e.error().c_str(), e.argId().c_str());
         return EXIT_FAILURE;
-    }
-    catch (...)
-    {
-        fprintf(stderr, "[ERROR] An unknown exception occurred!\n");
-        return EXIT_FAILURE;
-    }
-
-    try
-    {
-        GenImageTool::Parser parser;
-        GenesisObjects genesisObjects = parser.parse(inputFile, graphicsDir);
-
-        GenImageTool::Writer writer;
-        writer.write(genesisObjects, outputDir);
-
-        printf("Done!\n");
     }
     catch (const std::runtime_error& err)
     {
@@ -66,5 +56,6 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
+    printf("Done!\n");
     return EXIT_SUCCESS;
 }
